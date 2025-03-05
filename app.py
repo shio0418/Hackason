a11720ea8f07fb8559604d404764a62d0dcf2871
shio0418 <furumiya.s.aa@m.titech.ac.jp>
2025-03-05 17:25:16 +0900

    reply

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
+
+import sqlite3
 # Flaskアプリケーションのインスタンスを作成
 app = Flask(__name__)
 
@@ -16,7 +18,59 @@ init_db()
 # ルートURLにアクセスしたときの処理
 @app.route('/')
 def home():
+    data = request.json
+
     return 'Hello, Flask!'
+
+# データベースに接続し、データを挿入する関数
+def insert_post(text):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO posts (text) VALUES (?)', (text,))
+    conn.commit()
+    conn.close()
+
+def insert_post_reply(post_id,text):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('INSERT INTO replies (post_id,text) VALUES (?,?)', (post_id,text,))
+    conn.commit()
+    conn.close()
+
+# 投稿
+@app.route("/posts", methods=["POST"])
+def add_post():
+    ##ここを考える
+    data = request.json
+    text = data.get('text')
+    if text:
+        insert_post(text)
+        return jsonify({'message': 'Post created successfully'}), 201
+    else:
+        return jsonify ({'message': 'No text provided'}), 400
+
+    
+# 大喜利回答
+@app.route("/replies", methods=["POST"])
+def add_reply():
+    data = request.json
+    text = data.get('text')
+    post_id = data.get('post_id')
+    if post_id < 0:
+        return jsonify({'message': 'Invalid post_id'}), 400
+    elif text:
+        insert_post_reply(post_id,text)
+        return jsonify({'message': 'Post created successfully'}), 201
+    else:
+        return jsonify ({'message': 'No text provided'}), 400
+
+# いいね
+@app.route("/posts/<int:post_id>/like", methods=["POST"])
+def like_post(post_id):
+    #ここを考える
+    return
+
+
 # アプリケーションを実行
 if __name__ == '__main__':
     app.run(debug=True)
