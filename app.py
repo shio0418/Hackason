5606c0c705c593fa30c517b0c396a6c809403eae
isumi-100 <kk.100.koizu@gmail.com>
2025-03-06 12:31:02 +0900

    userシステムなし

--- app.py
+++ app.py
@@ -5,14 +5,6 @@ import sqlite3
 app = Flask(__name__)
 app.secret_key = 'key'
 
-# DB初期化
-def init_db():
-    with sqlite3.connect("database.db") as conn:
-        c = conn.cursor()
-        c.execute("CREATE TABLE IF NOT EXISTS posts (id INTEGER PRIMARY KEY, text TEXT, likes INTEGER DEFAULT 0)")
-        c.execute("CREATE TABLE IF NOT EXISTS replies (id INTEGER PRIMARY KEY, post_id INTEGER, text TEXT, votes INTEGER DEFAULT 0)")
-        c.execute("CREATE TABLE IF NOT EXISTS daily_theme (date TEXT, post_id INTEGER)")
-        conn.commit()
 
 init_db()
 
